d778d9852017d71b816dda0c3529ed68cd383520
Naflaaaaajty <1773239694@qq.com>
2022-10-26 10:28:02 +0800

    大一上

--- code/大一上/无序数组查找元素/源.c
+++ code/大一上/无序数组查找元素/源.c
@@ -1,14 +1,57 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
+void find(int arr[], int zuo, int you, int a, int mid); 
+void bubble(int arr[], int sz);
 int main()
 {
-	int arr[200] = { 0 };
+	int arr[] = { 1,6,4,9,7,3,45,97,23,46,87,122,2,46,987,124,569};
 	int i;
-	do
-	{
-		printf("请输入要输入多少数字进入数组\n");
-		scanf("%d")
-	} while (i);
+	int sz = sizeof(arr) / sizeof(arr[0]) - 1;
+	int zuo = 0, you = sizeof(arr) / sizeof(arr[0]) - 1, a=0;
+	int mid = 0;
+	bubble(arr,sz);
+	find(arr,zuo, you, a, mid);
 	system("pause");
 	return 0;
+}
+void find(int arr[], int zuo, int you, int a, int mid)
+{
+	scanf("%d", &a);
+	while (zuo <= you)
+	{
+		mid = (zuo + you) / 2;
+		if (arr[mid] > a)
+		{
+			you = mid - 1;
+		}
+		else if (arr[mid] < a)
+		{
+			zuo = mid + 1;
+		}
+		else
+			break;
+	}
+	if (zuo <= you)
+		printf("找到了,下标是%d\n", mid);
+	else
+		printf("找不到\n");
+}
+void bubble(int arr[], int sz)
+{
+	int jh, count = 1;
+	for (int i = 0; i < sz; i++)
+	{
+		for (int j = 0; j < sz - i; j++)
+		{
+			if (arr[j] > arr[j + 1])
+			{
+				jh = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = jh;
+				count++;
+			}
+		}
+		if (count == 1)
+			break;
+	}
 }
--- code/大一上/调试/d.c
+++ code/大一上/调试/d.c
@@ -1,12 +1,22 @@
-#define _CRT_SECURE_NO_WARNINGS
-#include<stdio.h>
+//#define _CRT_SECURE_NO_WARNINGS
+//#include<stdio.h>
+//#include<math.h>
+//int main()
+//{
+//	float a=0.0;
+//	for (int i = 1; i <= 100; i++)
+//	{
+//		a += (pow(-1, i + 1) / i) ;
+//	}
+//	printf("%f", a);
+//	system("pause");
+//	return 0;
+
+#include <stdio.h>
+int num = 10;
 int main()
 {
-	double a;
-	for (int i = 0; i < 100; i++)
-	{
-
-	}
-	system("pause");
-	return 0;
+    int num = 1;
+    printf("num = %d\n", num);
+    return 0;
 }
